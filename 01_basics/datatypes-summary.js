5d29cd95ed282f24e529098c774041f4a1ecdb1b
Saswat  Kumar Upadhyaya <136928747+saswat-07@users.noreply.github.com>
2024-02-05 18:49:40 +0000

    datatypes added

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -6,10 +6,10 @@
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNum = 23442223332323245523333n
-console.log(typeof bigNum);
+// console.log(typeof bigNum);
 
 
 // Reference type (Non Primitive)
@@ -26,7 +26,31 @@ const myFunction = function(){
     console.log("Hello");
 }
 
-console.log(typeof myFunction); //object function
-console.log(typeof heros);
-console.log(typeof myObj);
-console.log(typeof id);
+// console.log(typeof myFunction); //object function
+// console.log(typeof heros);
+// console.log(typeof myObj);
+// console.log(typeof id);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* Stack (Primitive)(copy of original value, any changes made will reflect to copy not to original)
+  , Heap Memory (Non-Primitive)(reference of original value,any changes made will reflect to original)
+*/
+
+let myName = "saswat"
+let anotherName = myName
+anotherName = "chaiaurcode"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "sas@email.com",
+    upi: "payment@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "user@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
