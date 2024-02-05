a8f9945a1c86d93548b4d0dead2b318cd0e1ccd2
Saswat  Kumar Upadhyaya <136928747+saswat-07@users.noreply.github.com>
2024-02-05 17:25:28 +0000

    new file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let num = 33
+// console.log(typeof num);
+// console.log(typeof (num));
+
+let score = "33abc" //or null/undefined/true/"33"
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;false => 0
+
+let isLoggedIn = 1 //or ""/"saswat"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// "" => false
+// "saswat" => true
+
+let someNumber = 31
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
