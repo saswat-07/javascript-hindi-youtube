63de2ae17ecf312ee82e6f06d24fd524f5ae978e
Saswat  Kumar Upadhyaya <136928747+saswat-07@users.noreply.github.com>
2024-02-05 17:50:59 +0000

    new

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,15 +15,53 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1 //or ""/"saswat"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // "" => false
 // "saswat" => true
 
 let someNumber = 31
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*======================================= OPERATIONS ==================================== */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " saswat"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); //bad practice
+
+// console.log((3 + 4) * 5 % 3);
+
+let num1, num2, num3
+num1 = num2 = num3 = 4 
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
 
 
 
